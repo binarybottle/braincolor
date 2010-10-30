473168258e711ff25ac316f649766d940a11df94
binarybottle <arno@binarybottle.com>
2010-10-29 23:18:35 -0400

    fixed bug -- graph nodes now ordered correctly

--- braincolorgraph.py
+++ braincolorgraph.py
@@ -7,25 +7,43 @@ import itertools
 from colormath.color_objects import LCHuvColor
 from braingraph import G
 
-plot_original_graph = 0
+plot_whole_colormap = 1
+plot_whole_graph = 0
 plot_colormap = 0
-plot_graph = 1
+plot_graph = 0
 make_xml = 0
 
-run_permutations = 1
-  
 Ntotal = G.number_of_nodes()
 color_angle = 360.0/Ntotal
-Lumas_init = np.arange(20,80,20)
-incr_Luma = 5
+Lumas_init = np.arange(40,70,10)
 init_angle = 0
-chroma = 90
+chroma = 100
 number_min = 100
 number_max = 600
 step = 10
 
+if plot_colormap * plot_graph * make_xml > 0:
+    run_permutations = 1
+else: 
+    run_permutations = 0
+
+# Plot the colormap for the whole graph    
+if plot_whole_colormap:
+    fig1 = plt.figure(figsize=(5,10))
+    # Define colormap as uniformly distributed colors in CIELch color space
+    Lumas = Lumas_init.copy()
+    while len(Lumas) < Ntotal: 
+        Lumas = np.hstack((Lumas,Lumas_init))
+    hues = np.arange(init_angle, init_angle + Ntotal*color_angle, color_angle)
+    for iN in range(Ntotal):
+        ax = plt.subplot(Ntotal, 1, iN+1)
+        plt.axis("off")
+        lch = LCHuvColor(Lumas[iN], chroma, hues[iN]) #print(lch)
+        rgb = lch.convert_to('rgb', debug=False)
+        plt.barh(0,50,1,0, color=[rgb.rgb_r/255.,rgb.rgb_g/255.,rgb.rgb_b/255.])
+        
 # Plot whole graph, with subgraphs in different colors
-if plot_original_graph:
+if plot_whole_graph:
     pos = nx.graphviz_layout(G,prog="neato")
     subG = nx.connected_component_subgraphs(G)
     colors = ['cyan','pink','yellow','tan','white']
@@ -50,7 +68,6 @@ for number_start in range(number_min,number_max,step):
         # Define colormap as uniformly distributed colors in CIELch color space
         Lumas = Lumas_init.copy()
         while len(Lumas) < N: 
-            Lumas_init += incr_Luma
             Lumas = np.hstack((Lumas,Lumas_init))
         hues = np.arange(init_angle, init_angle + N*color_angle, color_angle)
         init_angle += N*color_angle
@@ -59,25 +76,29 @@ for number_start in range(number_min,number_max,step):
         if run_permutations:
             # Convert subgraph into an adjacency matrix (1 for adjacent pair of regions)
             neighbor_matrix = np.array(nx.to_numpy_matrix(g))
-            sys.exit()
+            matrix_sum = np.sum(neighbor_matrix, axis=0)
+            neighbor_matrix = neighbor_matrix * (matrix_sum * np.ones((N,N))).transpose()
+            #sys.exit()
             # Compute permutations of colors and color pair differences
-            DEmin = np.Inf
+            DEmax = 0
             permutations = [np.array(s) for s in itertools.permutations(range(0,N),N)]
+            permutation_max = np.zeros(N)
             for ipermutations in range(len(permutations)):
                 permutation = permutations[ipermutations]
                 color_delta_matrix = np.zeros(np.shape(neighbor_matrix))   
                 for i1, icolor1 in enumerate(permutation):
                     lch1 = LCHuvColor(Lumas[icolor1],chroma,hues[icolor1]) 
                     for i2, icolor2 in enumerate(permutation):
-                        if i2 > i1:
+                        if (i2 > i1) and (neighbor_matrix[i1,i2] > 0):
                             lch2 = LCHuvColor(Lumas[icolor2],chroma,hues[icolor2])
                             DE = lch1.delta_e(lch2, mode='cie2000')
                             color_delta_matrix[i1,i2] = DE   
                 DE = np.sum((color_delta_matrix * neighbor_matrix))
                 # Store the color permutation with the minimum adjacency cost
-                if DE < DEmin:
-                    DEmin = DE
-                    permutation_min = permutation
+                if DE > DEmax:
+                    DEmax = DE
+                    permutation_max = permutation
+                    #color_delta_matrix_max = color_delta_matrix
                
         # Plot the reordered colormap for the subgraph    
         if plot_colormap:
@@ -86,29 +107,28 @@ for number_start in range(number_min,number_max,step):
             for iN in range(N):
                 ax = plt.subplot(N, 1, iN+1)
                 plt.axis("off")
-                ic = permutation_min[iN]
+                ic = permutation_max[iN]
                 lch = LCHuvColor(Lumas[ic],chroma,hues[ic]) #print(lch)
                 rgb = lch.convert_to('rgb', debug=False)
                 plt.barh(0,50,1,0, color=[rgb.rgb_r/255.,rgb.rgb_g/255.,rgb.rgb_b/255.])
-                pos = list(ax.get_position().bounds)
-    
         # Draw a figure of the colored subgraph
         if plot_graph:
             pos = nx.graphviz_layout(G,prog="neato")  #nx.spring_layout(G)
             #nx.draw(G, pos, node_size=1200, node_color='cyan') #, hold=True)
             for iN in range(N):
-                ic = permutation_min[iN]
+                ic = permutation_max[iN]
                 lch = LCHuvColor(Lumas[ic],chroma,hues[ic]) #print(lch)
                 rgb = lch.convert_to('rgb', debug=False)
                 color = [rgb.rgb_r/255.,rgb.rgb_g/255.,rgb.rgb_b/255.]
                 nx.draw_networkx_nodes(g, pos, node_size=1200, nodelist=[g.node.keys()[iN]], node_color=color) #, hold=True)
                 nx.draw_networkx_edges(g, pos, alpha=0.75, width=2)
                 nx.draw_networkx_labels(g, pos, font_size=10, font_color='white')
-           
+            #sys.exit()
+            
         # Generate XML output       
         if make_xml:
             for iN in range(N):
-                ic = permutation_min[iN]
+                ic = permutation_max[iN]
                 lch = LCHuvColor(Lumas[ic],chroma,hues[ic]) #print(lch)
                 rgb = lch.convert_to('rgb', debug=False)
                 color = [rgb.rgb_r, rgb.rgb_g, rgb.rgb_b]
