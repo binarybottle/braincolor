1a068f97d225f023714bcc3f4aa2df221272d17b
= <arno@binarybottle.com>
2015-03-01 17:51:24 -0800

    Swap index pages

---
+++ protocols/x/index.php
@@ -0,0 +1,272 @@
+<html>
+<head>
+<?php include_once("./shared/metatags.php"); ?>
+<link rel="stylesheet" type="text/css" href="shared/style.css"> 
+ 
+<script type="text/javascript" src="./scripts/popups.js"></script>
+
+</head>
+ <body>
+
+<?php include_once("./shared/banner.php"); ?>
+
+<title>About brainCOLOR</title>
+
+<div class="main">
+
+<!--font size="5">About brainCOLOR</font-->
+
+<font size="5">BrainCOLOR</font>
+is interested in brain anatomical labels and labeling methods.
+
+<br />
+<br /> 
+<br /> 
+
+<table>
+<tr>
+<b>Projects</b>
+</tr>
+<hr>
+<tr>
+<td width="30"></td>
+<td valign="top">
+<a href="protocols">labels</a> 
+<br /> 
+<br /> 
+<a href="colors">colors</a> 
+</td>
+
+<td width="10"></td>
+
+<td valign="top">
+and anatomical labeling protocols for MRI brain volumes <a href="roygbiv">(example)</a> 
+<br /> 
+<br /> 
+optimized for labeling/viewing groups of adjacent regions
+</td>
+</tr>
+</table>
+
+<br /><br />
+
+<table>
+<tr>
+<b>News</b> 
+</tr>
+<hr>
+<tr>
+<td width="20"></td>
+<td valign="top">
+
+<!--i>July 2011:</i>&nbsp;&nbsp;
+20 labeled OASIS brain images added to the <a href="roygbiv"><font color="red">R</font><font color="FF9900">O</font><font color="FFCC00">Y</font><font color="green">G</font> <font color="blue">B</font>rain <font color="6600FF">I</font>mage <font color="purple">V</font>iewer</font></a>
+
+<br />
+<br />
+-->
+
+<i>Aug. 2011:</i>&nbsp;&nbsp;
+First application of the finalized BrainCOLOR protocol: 20 repeat-scan OASIS brain images.
+
+<br />
+<br />
+
+<i>Nov. 2010:</i>&nbsp;&nbsp;
+Society for Neuroscience <a href="colors">poster</a>:
+<span class="xsmalltext">
+&quot;An interactive tool for constructing optimal brain colormaps&quot;
+</span>
+
+<br />
+<br />
+
+<i>Jun. 2010:</i>&nbsp;&nbsp;
+Human Brain Mapping <a href="docs/OpenLabels_HBM2010_poster_ArnoKlein.pdf" onClick="return popup(this,'HBM2010poster')">poster</a>:
+<span class="xsmalltext">
+&quot;Open labels: online feedback for a public resource of manually labeled brain images&quot;
+</span>
+<br />
+<br />
+
+<i>Feb. 2010:</i>&nbsp;&nbsp;
+<a href="http://groups.google.com/group/braincolor">Google group</a> to discuss labels, labeling methods, color maps, etc.
+
+<br />
+<br />
+
+<i>Nov. 2009:</i>&nbsp;&nbsp;
+<a href="docs/brainCOLOR_meeting_20091102.pdf" onClick="return popup(this,'meeting1')">First meeting</a>, held at Neuromorphometrics, Inc.
+
+<br />
+<br />
+
+<i>Aug. 2009:</i>&nbsp;&nbsp;
+Surveys administered to solicit feedback about our labeling protocol.
+
+</table>
+
+<br /><br />
+
+<table>
+<tr>
+<b>People</b>
+</tr>
+<hr>
+<tr>
+
+<td width="30"></td>
+
+<td valign="top">
+
+<table>
+<tr>
+<td valign="top">
+Organizer &amp; website:
+</td>
+<td width="20">
+</td>
+<td>
+<a href="http://www.binarybottle.com" onClick="return popup(this,'arno')">
+Arno Klein</a>
+<br />
+<span class="xsmalltext">
+Sage Bionetworks, Seattle, WA
+</span>
+<br />
+<br />
+</td>
+</tr>
+
+<tr>
+<td valign="top">
+Labeling leader:
+</td>
+<td width="20">
+</td>
+<td>
+Andrew Worth
+<br />
+<span class="xsmalltext">
+<a href="http://www.neuromorphometrics.com" onClick="return popup(this,'neuromorphometrics')">
+Neuromorphometrics</a>, Inc., MA
+</span>
+<br />
+<br />
+</td>
+</tr>
+
+<tr>
+<td valign="top">
+Labeling protocol:
+</td>
+<td width="20">
+</td>
+<td>
+Jason Tourville
+<br />
+<span class="xsmalltext">
+Dept. of Cognitive and Neural Systems, Boston University
+</span>
+<br />
+<br />
+Ruth Carper
+<br />
+<span class="xsmalltext">
+Center for Human Development, UCSD
+</span>
+<br />
+<br />
+Georges Salamon
+<br />
+<span class="xsmalltext">
+Dept. of Radiology, David Geffen School of Medicine, UCLA
+</span>
+<br />
+<br />
+</td>
+</tr>
+
+<tr>
+<td valign="top">
+More members:
+</td>
+<td width="20">
+</td>
+<td>
+Greg Millington
+<br />
+<span class="xsmalltext">
+Neuromorphometrics, Inc.
+</span>
+<br />
+<br />
+David Kennedy
+<br />
+<span class="xsmalltext">
+Univ. of Massachusetts Medical Center, Wooster, MA
+</span>
+<br />
+<br />
+Satrajit S. Ghosh
+<br />
+<span class="xsmalltext">
+Research Laboratory of Electronics, MIT
+</span>
+<br />
+<br />
+Bennett Landman
+<br />
+<span class="xsmalltext">
+Vanderbilt University, Nashville, TN
+</span>
+<br />
+<br />
+Steve Pieper
+<br />
+<span class="xsmalltext">
+Isomics, Inc., MA
+</span>
+<br />
+<br />
+David Shattuck
+<br />
+<span class="xsmalltext">
+UCLA, CA
+</span>
+<br />
+<br />
+</td>
+</tr>
+
+</table>
+
+</td>
+</tr>
+</table>
+
+<table>
+<tr>
+<b>Funding</b>
+</tr>
+<hr>
+<tr>
+<td width="20"></td>
+<td valign="top">
+<a href="http://www.neuromorphometrics.com" onClick="return popup(this,'neuromorphometrics')">
+Neuromorphometrics</a> is being funded by grant R43 MH084358 from the NIMH.
+<br />
+<br />
+<a href="http://www.binarybottle.com" onClick="return popup(this,'arno')">Arno Klein</a> is the P.I. of NIMH-funded R01 grant MH084029.
+</td>
+</tr>
+</table>
+
+</div>
+
+
+<?php include_once("./shared/footer.php"); ?>
+
+</div>
+</div>
+
